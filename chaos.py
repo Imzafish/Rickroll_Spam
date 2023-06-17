1891b44f005a07143c5e11273cc37c352d4f5e39
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-06-18 08:36:26 +1000

    r now breaks the loop

--- chaos.py
+++ chaos.py
@@ -1,7 +1,9 @@
 import webbrowser
 import time
+import keyboard
 
 while True:
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
-time.sleep(0.1)
+    if keyboard.is_pressed('r'):
+        break
 exit()
