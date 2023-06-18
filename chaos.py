2e1c0df26f8abe95a9329d0da314721f42cbd80d
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-06-18 16:32:13 +1000

    Had to remove audio as it didn't work

--- chaos.py
+++ chaos.py
@@ -1,15 +1,6 @@
 import webbrowser
 import time
 import keyboard
-import ctypes
-from pycaw.pycaw import AudioUtilities
-
-# Get the default audio interface
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(ctypes.c_ulonglong(0), ctypes.c_void_p(None), None)
-
-# Get the volume control
-volume = interface.QueryInterface(ctypes.POINTER(ctypes.c_float))
 
 def disable_alt_f4(e):
     if e.event_type == keyboard.KEY_DOWN:
@@ -23,10 +14,9 @@ while True:
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
     if keyboard.is_pressed('r'):
         break
-    volume.SetMasterVolumeLevel(0, None)  # Set volume to maximum (1.0)
     time.sleep(0.1)
 
 # Unregister the event handler when the loop ends
 keyboard.unhook(disable_alt_f4)
-exit()
+
 
