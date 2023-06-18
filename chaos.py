6b51e0cf132ca1f4623bfbfe36f05d5db3c88669
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-06-18 16:39:51 +1000

    RIP WORK

--- chaos.py
+++ chaos.py
@@ -1,22 +1,9 @@
 import webbrowser
 import time
 import keyboard
-
-def disable_alt_f4(e):
-    if e.event_type == keyboard.KEY_DOWN:
-        if e.name == 'alt' and keyboard.is_pressed('f4'):
-            return False  # Block the Alt+F4 combination
-
-# Register the event handler to block Alt+F4
-keyboard.hook(disable_alt_f4)
-
 while True:
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1)
     if keyboard.is_pressed('r'):
         break
-    time.sleep(0.1)
-
-# Unregister the event handler when the loop ends
-keyboard.unhook(disable_alt_f4)
 
 
