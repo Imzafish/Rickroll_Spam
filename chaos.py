b77a6faf9b9f32837b0a9478adb82ddfd9c5b7de
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-06-18 16:55:19 +1000

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -6,9 +6,9 @@ mouse = Controller()
 
 while True:
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1) #Spams the rickrolls
-    if keyboard.is_pressed('r'):
+    if keyboard.is_pressed('r'): #ALT+F4 closes the current window but not the others so an overide is needed
         break
     mouse.position = (0, 0)  # Move the mouse to (0,0) to disable clicks
-    time.sleep(0.1)  # Add a small delay to avoid high CPU usage #Remove if you want utter chaos
+    time.sleep(0.1)  # Add a small delay to avoid exploding CPU usage #Remove if you want utter chaos
 
 exit()
