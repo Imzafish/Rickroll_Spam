c04dba8e0a17d17bc2febfb08a92fc877cf76d8e
Max Worrall <max.worrall@education.nsw.gov.au>
2023-07-03 15:21:55 +1000

    fixed stuff with the exe! All (but the mouse) now works!

--- chaos.py
+++ chaos.py
@@ -1,14 +1,15 @@
 import webbrowser
 import time
 import keyboard
-from pynput.mouse import Controller
-mouse = Controller()
+#import pynput
+#mouse = pynput.mouse.Controller()
+
+#Imma sry Riley the moving the mouse don't work
+#Try a different library
 
 while True:
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", new=1) #Spams the rickrolls
     if keyboard.is_pressed('r'): #ALT+F4 closes the current window but not the others so an overide is needed
         break
-    mouse.position = (0, 0)  # Move the mouse to (0,0) to disable clicks
+    #mouse.position = (0, 0)  # Move the mouse to (0,0) to disable clicks
     time.sleep(0.1)  # Add a small delay to avoid exploding CPU usage #Remove if you want utter chaos
-
-exit()
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup # Need this to handle modules
-import py2exe 
-import webbrowser # Now we need this to import the required methods/functions so the toexe can parse it!
-import time
-import keyboard
-from pynput.mouse import Controller
+import py2exe
 from shutil import rmtree
+import sys, os
+
+packages = ['keyboard', 'time', 'webbrowser']
+
+sys.argv.append('py2exe')
 
 try: # remove the temporary folders from last build bcos i don't know what will happen if they stay
     rmtree('dist/')
@@ -14,12 +15,18 @@ except:
 
 try:
     setup(
-        options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-        console = [{'script': "chaos.py"}],
+        name='Chaos',
+        version='1.0',
+        description='Do not run at all costs... unless you are dying of curiosity. Will not delete anything on your laptop.',
+        options = {'py2exe': {"includes": packages,
+                            "bundle_files": 1,
+                            'compressed': True}},
+        console = ["chaos.py"],
         zipfile = None,
     )
 except SystemExit:
     input('Please try to run toexe.bat as administrator, not this as a normal python file!\nPress enter to exit')
+    exit()
 
 print('Now there should be a chaos.exe file in the dist folder! Check it out!')
 if input('Press enter to exit or 1 to remove all the temporary junk we don\'t need') == '1':
